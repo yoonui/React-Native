631ccde516dfb8b0f3f8c5e2d433c306a7245e4e
yoonui <yoony0612@ajou.ac.kr>
2022-02-24 20:56:40 +0900

    3_User Log in Screen 완료

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## 2022년 2월 24일
 * 사용자 로그인 화면 제작 중
-* [#2 User Log in Screen](https://youtu.be/wZe-3jQ74C0)
+** [#2 User Log in Screen](https://youtu.be/wZe-3jQ74C0)
+** [#3 User Log in Screen](https://youtu.be/FGxlrpti35A)
--- src/screens/authScreens/SignInScreen.js
+++ src/screens/authScreens/SignInScreen.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, Text, StyleSheet, Dimensions, TextInput} from 'react-native';
 import {colors, parameters} from "../../global/styles";
 import {Icon} from 'react-native-elements';
 import Header from '../../components/Header';
+import * as Animatable from 'react-native-animatable';
 
 export default function SignInScreen(){
+
+    const [textInput2Fossued, setTextInputFossued] = useState(false);
+
+    const textInput1 = useRef(1);
+    const textInput2 = useRef(2);
+
     return (
         <View style={styles.container}>
             <Header title="MY ACCOUNT" type="arrow-left"/>
-            <View>
-                <Text style={title}>Sign-In</Text>
+            <View style={{marginLeft:20, marginTop:10}}>
+                <Text>Sign-In</Text>
             </View>
-            <View>
+            <View style={{alignItems:"center", marginTop:10}}>
                 <Text style={styles.text1}>Please enter the email and password</Text>
+                <Text style={styles.text1}>registered with your account</Text>
+            </View>
+            <View style={{marginTop:20}}>
+                <View>
+                    <TextInput
+                        style={styles.TextInput1}
+                        placeholder="Email"
+                        ref = {textInput1}
+                    />
+                </View>
+                <View style={styles.TextInput2}>
+                    <Animatable.View animation={textInput2Fossued ? "" : "fadeInLeft"} duration = {400}>
+                        <Icon
+                            name="lock"
+                            iconStyle={{color:colors.grey3}}
+                            type="material"
+                            style={{}}
+                        />
+                    </Animatable.View >
+                    <TextInput
+                        style={{width:"80%"}}
+                        placeholder="Password"
+                        ref = {textInput2}
+                        onFocus = {()=>{
+                            setTextInputFossued(false);
+                        }}
+                        onBlur = {()=>{
+                            setTextInputFossued(true);
+                        }}
+                    />
+                    <Animatable.View animation={textInput2Fossued ? "" : "fadeInLeft"} duration = {400}>
+                        <Icon
+                            name="visibility-off"
+                            iconStyle={{color:colors.grey3}}
+                            type="material"
+                            style={{marginRight:10}}
+                        />
+                    </Animatable.View>
+                </View>
             </View>
         </View>
     );
@@ -23,7 +69,26 @@ const styles = StyleSheet.create({
         flex : 1
     },
     text1:{
-        color : colors.grey4,
+        color : colors.grey3,
         fontSize : 16
+    },
+    TextInput1:{
+        borderWidth:1,
+        borderColor:"#86939e",
+        marginHorizontal:20,
+        borderRadius:12,
+        marginBottom:20,
+        paddingLeft:15
+    },
+    TextInput2:{
+        borderWidth:1,
+        borderRadius:12,
+        marginHorizontal:20,
+        borderColor:"#86939e",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignContent:"center",
+        alignItems:"center",
+        paddingLeft:15
     }
 });
